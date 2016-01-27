be4086ba18cbe6beb2d935f3d587ee6fac360faa
Lars <lars@Larss-Air.fritz.box>
2016-01-27 16:44:53 +0100

    Initial commit

---
+++ NuimoSimulator/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NuimoSimulator
+//
+//  Created by Lars on 27.01.16.
+//  Copyright © 2016 Senic GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
